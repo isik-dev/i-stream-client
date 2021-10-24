7a52e278828bbb78ff8ca82ac097b4ef6e73a8c3
islomnumanov <islom4ik.numanov@gmail.com>
2021-10-24 21:49:02 +0900

    redux-form, form submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import StreamList from "./streams/StreamList";
-import StreamDelete from "./streams/StreamDelete";
-import StreamEdit from "./streams/StreamEdit";
-import StreamShow from "./streams/StreamShow";
 import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 import Header from "./Header";
 
-// has divided all of the streams comps into a different file
 const App = () => {
   return (
     <div className="ui container">
