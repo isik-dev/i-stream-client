c294cc5b55be6176cbf10268a1e09f19f8586cd2
islomnumanov <islom4ik.numanov@gmail.com>
2021-10-28 12:29:31 +0900

    from old classic a tags to react route

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const PageOne = () => {
+  return (
+    <div>
+      This is a bad approach of navigating!
+      <br />
+      This is page 1<a href="/pagetwo">Go to page two</a>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      This is a bad approach of navigating!
+      <br />
+      This is page 2<button>Click Me!</button>
+      <a href="/">Go to page one</a>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/pagetwo" component={PageTwo} />
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
