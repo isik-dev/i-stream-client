78f61c70a22e58db61408d05d90abe74a823576c
islomnumanov <islom4ik.numanov@gmail.com>
2021-10-28 14:09:14 +0900

    window.gapi.load & window.gapi.client.init

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,22 @@
 import React from "react";
+// when the component first renders to the screen we will load google auth library as follows:
+// componentDidMount() { window.gapi.load('client:auth2') }
+
+// loading a library will take some time, therefore we need to wait and when it loads we go the next step
+// we do it using a callback function:
+// componentDidMount() { window.gapi.client('client:auth2', () => {}) }
+
+// inside that callback function we should pass clientId and scope, so that later we can initialize GoogleAuth object!
 
 class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId: process.env.REACT_APP_GOOGLE,
+        scope: "email",
+      });
+    });
+  }
   render() {
     return <div>GoogleAuth</div>;
   }
