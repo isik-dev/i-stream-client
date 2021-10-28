6f1ea78614e60e2208cd20dcc272834889eea89f
islomnumanov <islom4ik.numanov@gmail.com>
2021-10-28 13:03:54 +0900

    scaffolded components and wired them using Route

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-
-const PageOne = () => {
-  return (
-    <div>
-      This is a good approach of navigating!
-      <br />
-      This is page 1<Link to="/pagetwo">Go to page two</Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      This is a bad approach of navigating!
-      <br />
-      This is page 2<button>Click Me!</button>
-      <a href="/">Go to page one</a>
-    </div>
-  );
-};
+import StreamEdit from "./streams/StreamEdit";
+import StreamCreate from "./streams/StreamCreate";
+import StreamShow from "./streams/StreamShow";
+import StreamList from "./streams/StreamList";
+import StreamDelete from "./streams/StreamDelete";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div>
-        <Route path="/" exact component={PageOne} />
-        <Route path="/pagetwo" component={PageTwo} />
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" exact component={StreamCreate} />
+        <Route path="/streams/show" exact component={StreamShow} />
+        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/delete" exact component={StreamDelete} />
       </div>
     </BrowserRouter>
   );
