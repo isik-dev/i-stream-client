25c1b164b2d9b828ffa2890a1f2cc788c9e7fe21
islomnumanov <islom4ik.numanov@gmail.com>
2021-10-28 13:18:02 +0900

    Header fixed as nav bar, usign link outside of routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,26 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import StreamEdit from "./streams/StreamEdit";
 import StreamCreate from "./streams/StreamCreate";
 import StreamShow from "./streams/StreamShow";
 import StreamList from "./streams/StreamList";
 import StreamDelete from "./streams/StreamDelete";
+import Header from "./Header";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Route path="/" exact component={StreamList} />
-        <Route path="/streams/new" exact component={StreamCreate} />
-        <Route path="/streams/show" exact component={StreamShow} />
-        <Route path="/streams/edit" exact component={StreamEdit} />
-        <Route path="/streams/delete" exact component={StreamDelete} />
-      </div>
-    </BrowserRouter>
+    <div className="ui container">
+      <BrowserRouter>
+        <Header />
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/show" exact component={StreamShow} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+        </div>
+      </BrowserRouter>
+    </div>
   );
 };
 
