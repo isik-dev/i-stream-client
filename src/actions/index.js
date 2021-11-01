503496059aee02b6017eaa0b725ab218d0d6c741
islomnumanov <islom4ik.numanov@gmail.com>
2021-11-01 22:12:47 +0900

    on to redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
 import { SIGN_IN, SIGN_OUT } from "./types";
 // Below are two very simple functions that are referred to as action creators that will return an object with single values!!
-export const signIn = () => {
+export const signIn = (userId) => {
   return {
     type: SIGN_IN,
+    payload: userId,
   };
 };
 
